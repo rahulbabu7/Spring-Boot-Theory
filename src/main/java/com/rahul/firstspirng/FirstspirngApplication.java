00419d5661c2afcf57d2a9344d64d0f876036f12
rahulbabu7 <rahulbabu436@gmail.com>
2025-05-28 12:23:19 +0530

    added springBootApplication anotation equalent

--- src/main/java/com/rahul/firstspirng/FirstspirngApplication.java
+++ src/main/java/com/rahul/firstspirng/FirstspirngApplication.java
@@ -4,6 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.context.ApplicationContext;
 
+
+/*
+ * 
+ * 
+ * @SpringBootApplication
+public class MyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApp.class, args);
+    }
+}
+This is equivalent to:
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class MyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApp.class, args);
+    }
+}
+
+
+
+ */
 @SpringBootApplication
 public class FirstspirngApplication {
 
